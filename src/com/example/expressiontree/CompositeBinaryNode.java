b2e60a21cb07d12619d4b2da1b58a9914230e085
Charles Araújo <charles.ma@samsung.com>
2014-04-03 11:11:44 -0400

    analise

--- src/com/example/expressiontree/CompositeBinaryNode.java
+++ src/com/example/expressiontree/CompositeBinaryNode.java
@@ -2,26 +2,28 @@ package com.example.expressiontree;
 
 /**
  * @class CompositeBinaryNode
- *
- * @brief Defines a left and right node (via inheritance).  It plays
- *        the role of a "Composite" in the Composite pattern.
+ * 
+ * @brief Defines a left and right node (via inheritance). It plays the role of
+ *        a "Composite" in the Composite pattern.
  */
 public class CompositeBinaryNode extends CompositeUnaryNode
 {
+
     /** Reference to the left child. */
     private ComponentNode left;
-  
+
     /** Ctor */
     public CompositeBinaryNode(ComponentNode left,
-                               ComponentNode right)
+        ComponentNode right)
     {
         super(right);
         this.left = left;
     }
 
     /** Return the left child. */
+    @Override
     public ComponentNode left()
     {
-	return left;
+        return left;
     }
 }
